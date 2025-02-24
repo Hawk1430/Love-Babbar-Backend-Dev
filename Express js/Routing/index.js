dd894faf539d86a68a6256e751d7e6ec45a13105
Prashant Raj <pr2125116@gmail.com>
2025-02-24 19:25:21 +0530

    first commit

---
+++ Express js/Routing/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Got a GET request");
+});
+app.post("/items", (req, res) => {
+  res.send("Got a POST request");
+});
+app.put("/items/:id", (req, res) => {
+  res.send("Got a PUT request");
+});
+app.delete("/items/:id", (req, res) => {
+  res.send("Got a DELETE request");
+});
+
+app.listen(port, () => {
+  console.log(`Hawk app listening on port ${port}`);
+});
