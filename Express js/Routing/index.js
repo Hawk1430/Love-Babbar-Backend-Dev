4c1165ca08d33e6efe26ecfe9df31151a4c1adf9
Prashant Raj <pr2125116@gmail.com>
2025-02-24 19:30:13 +0530

    Done the separate route folder

--- Express js/Routing/index.js
+++ Express js/Routing/index.js
@@ -2,18 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Got a GET request");
-});
-app.post("/items", (req, res) => {
-  res.send("Got a POST request");
-});
-app.put("/items/:id", (req, res) => {
-  res.send("Got a PUT request");
-});
-app.delete("/items/:id", (req, res) => {
-  res.send("Got a DELETE request");
-});
+// app.get("/", (req, res) => {
+//   res.send("Got a GET request");
+// });
+// app.post("/items", (req, res) => {
+//   res.send("Got a POST request");
+// });
+// app.put("/items/:id", (req, res) => {
+//   res.send("Got a PUT request");
+// });
+// app.delete("/items/:id", (req, res) => {
+//   res.send("Got a DELETE request");
+// });
+const birds = require("./routes/birds");
+
+app.use("/birds", birds);
 
 app.listen(port, () => {
   console.log(`Hawk app listening on port ${port}`);
